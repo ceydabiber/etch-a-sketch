c9f7e633b1193ffa6e508bedfd67b8e4b050ec54
ceydabiber <ceydabiber3@gmail.com>
2024-10-01 14:27:42 +0300

    implement resizing grid size property

--- web.js
+++ web.js
@@ -1,20 +1,31 @@
 const gridContainer = document.querySelector('.grid-container');
 const clearBtn = document.getElementById('clearBtn');
+const resizeBtn = document.getElementById('resizeBtn');
+const gridSizeInput = document.getElementById('grid-size'); 
 
 function  createGrid(size){
     const totalSquares = size*size;
+
+    const squareSize = 500 / size;
     
     gridContainer.innerHTML = '';
+
+    gridContainer.style.gridTemplateColumns = 'repeat(${size},  1fr)';
 
     for (let i = 0; i< totalSquares; i++){
         const square = document.createElement('div');
         square.classList.add('grid-square');
-        gridContainer.appendChild(square);
+
+        square.style.width =`${squareSize}px`;
+        square.style.height =`${squareSize}px`;
     
 
         square.addEventListener('mouseover', () => {
             square.style.backgroundColor = '#E6D9A2';
         });
+
+
+        gridContainer.appendChild(square);
 
    }
 }
@@ -26,6 +37,12 @@ function clearGrid(){
     });
 }
 
+function resizeGrid(){
+    const newSize = parseInt(gridSizeInput.value);
+    createGrid(newSize);
+}
+
 clearBtn.addEventListener('click', clearGrid);
+resizeBtn.addEventListener('click', resizeGrid);
 
 createGrid(16);
